1920164a1ff68c4f1db3b89ee8cea61b4e0e312a
Finx1 <49606811+Finx1@users.noreply.github.com>
2020-08-09 12:05:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,4 +9,16 @@ setInterval(function() {
 channel.send(` لعيون ماجيكو بنصير مليونيرات`);
 }, 30)
 })
-client.login(process.env.BOT_TOKEN);
+
+const Discord = require('discord.js');
+const client2 = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client2.on("ready", () => {
+let channel =     client2.channels.get("741953758614913075")
+setInterval(function() {
+channel.send(` لعيون ماجيكو بنصير مليونيرات`);
+}, 30)
+})
+client2.login(process.env.BOT_TOKEN2);
